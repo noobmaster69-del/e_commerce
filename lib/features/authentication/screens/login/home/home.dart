047d8c5875b7cca617df538c012e83699af995c2
Paulo Maala <maalapaulo@gmail.com>
2024-03-13 16:29:19 +0800

    commit

--- lib/features/authentication/screens/login/home/home.dart
+++ lib/features/authentication/screens/login/home/home.dart
@@ -1,6 +1,5 @@
-import 'package:e_commerce/common/widgets/custom_shapes/container/circular_container.dart';
-import 'package:e_commerce/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
-import 'package:e_commerce/utils/constants/colors.dart';
+import 'package:e_commerce/common/widgets/custom_shapes/container/primary_header_container.dart';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,33 +9,13 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(children: [
-          ECurvedWidgets(
-            child: Container(
-              color: EColors.primary,
-              padding: const EdgeInsets.all(0),
-              child: SizedBox(
-                height: 400,
-                child: Stack(
-                  children: [
-                    Positioned(
-                        top: -150,
-                        right: -250,
-                        child: CircularContainer(
-                            backGroundcolor:
-                                EColors.textWhite.withOpacity(0.1))),
-                    Positioned(
-                        top: 100,
-                        right: -300,
-                        child: CircularContainer(
-                            backGroundcolor:
-                                EColors.textWhite.withOpacity(0.1))),
-                  ],
-                ),
-              ),
+        child: Column(
+          children: [
+            PrimaryHeaderWidget(
+              child: Container(),
             ),
-          )
-        ]),
+          ],
+        ),
       ),
     );
   }
